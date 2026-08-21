623f43da043a5c79ef0927798d06e38c1772df46
Lalit Bhushan <lalitmuz2007@gmail.com>
2026-08-21 23:59:28 +0530

    ek baar fir se update

--- module/azurerm_resource_group/main.tf
+++ module/azurerm_resource_group/main.tf
@@ -1,9 +1,9 @@
 
 resource "azurerm_resource_group" "rgs" {
 
-    for_each= var.rgname
+  for_each = var.rgname
 
-    name= each.value.name
-    location= each.value.location
-  
+  name     = each.value.name
+  location = each.value.location
+
 }
